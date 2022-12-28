1bb2f63c0300d7b37f04216ba98345a579a41dd9
FatmaElSheibany <f.al-sheibany@hotmail.com>
2022-12-28 13:55:41 +0000

    fixed function 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,3 +1,6 @@
+#include "main.h"
+#include <stdio.h>
+
 /**
  * puts_half - prints second half of a string
  *
@@ -5,24 +8,33 @@
  *
  */
 
-#include "main.h"
-
 void puts_half(char *str)
 {
 	char *cpystr;
-	int len;
+	long int len;
 	int i;
 
+	cpystr = str;
 	len = 0;
 
 	/* iterate over a copy of the str so str pointer doesn't change position */
 	while (*cpystr != '\0')
 	{
-		len++;
+		len++; /* get length of str via counting non-\0 chars */
 		cpystr++;
 	}
+	_putchar('\n');
 
-	for (i = len / 2; i < len; i++)
+	/* if len is odd, print second half of str, skip middle char */
+	if (len % 2 == 0)
+		i = len / 2;
+	else
+		i = len / 2 + 1;
+
+	while (i < len)
+	{
 		_putchar(str[i]);
+		i++;
+	}
 	_putchar('\n');
 }
