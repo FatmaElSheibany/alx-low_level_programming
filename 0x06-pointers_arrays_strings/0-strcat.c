e4e4bd8ac3cd676170fb307033b4a0bb161ee350
FatmaElSheibany <f.al-sheibany@hotmail.com>
2023-01-01 21:16:57 +0000

    added function 0-strcat.c and test file 0-main.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+/**
+ * _strcat - concatenates two strings i.e. src onto dest
+ *
+ * @dest: destination string (first string)
+ * @src: string to concatenate onto dest (second string)
+ *
+ * Return: pointer to resulting string dest
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (*(dest + i) != '\0')
+		i++; /* go to end of dest string */
+
+	/* i now has the value of the index of null char */
+	/* (dest + i) is therefore address of null char of dest */
+
+	while (1)
+	{
+		*(dest + i) = *src; /* append src char onto dest */
+
+		if (*src == '\0') /* if reached null char, break */
+			break;
+		src++;
+		i++;
+	}
+
+	return (dest);
+}
