0dbe56af782aacd543c86862035544c36e61e0cd
FatmaElSheibany <f.al-sheibany@hotmail.com>
2023-01-02 18:01:57 +0000

    added function file 0-memset.c and testing file 0-main.c ; created main.h and updated it

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+/**
+ * This header file contains prototypes of all functions defined 
+ * in this project 0x07
+ *
+ */
+
+/* fills memory with a constant byte */
+char *_memset(char *s, char b, unsigned int n);
+
+
