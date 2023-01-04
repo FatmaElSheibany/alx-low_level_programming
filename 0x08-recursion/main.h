ead071c19254391dbf6fe3cbd4205d0f77445b86
FatmaElSheibany <f.al-sheibany@hotmail.com>
2023-01-04 14:24:00 +0000

    added function 0-puts_recursion.c ; added _putchar.c ; created the headerfile main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+/**
+ * This header file contains prototypes of all functions defined in this project; 
+ * including the _putchar.c
+ *
+ */
+
+int _putchar(char c);
+
+/* prints a string */
+void _puts_recursion(char *s);
+
+
