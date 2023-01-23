fdbb91d67e19f5c58b38e8395d2bd3abb1f80c5d
FatmaElSheibany <f.al-sheibany@hotmail.com>
2023-01-23 08:06:50 +0000

    added header file and program 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+/**
+ * main - program that prints its name
+ *
+ * @argc: number of arguments passed to main program in command line
+ * @argv: array of strings i.e. arguments passed to program in command line
+ *
+ * Return: 0 if success
+ *
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]); /*prints program neme*/
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+/**
+ * this is a header file containing prototypes of all functions
+ * defined as part of the 0x0A project
+ *
+ */
+
+int main(int argc, char* argv[]);
