bbf03caba01a11f1ef91ea936be479121c075c85
FatmaElSheibany <f.al-sheibany@hotmail.com>
2023-01-27 08:21:35 +0000

    added new file 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+/**
+ * create_array - creates an array of chars and initializes it with a specific char
+ *
+ * @size: size of array
+ * @c: the specific char
+ *
+ * Return: pointer to array if success; NULL if fails
+ * 
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
