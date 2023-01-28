e3477bcd549e4d3fde0d9fc7d8d36d0882124e5b
FatmaElSheibany <f.al-sheibany@hotmail.com>
2023-01-28 12:23:51 +0000

    added function 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,13 @@
+/**
+ * alloc_grid - returns a pointer to a 2D array of int initialized to 0
+ *
+ * @width: number of columns in the array
+ * @height: number of rows in the array
+ * 
+ * Return: pointer to the array if success else NULL
+ *
+ */
+
+#include <stdlib.h>
+
+
