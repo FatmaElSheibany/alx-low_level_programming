2df60aeffa91bbd24a5344b61f7fc5681acf8257
FatmaElSheibany <f.al-sheibany@hotmail.com>
2023-01-28 09:45:08 +0000

    wrote the function 0-create_array.c ; added testing file 0-main.c and the header file main.h

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,13 +1,37 @@
 /**
- * create_array - creates an array of chars and initializes it with a specific char
+ * create_array - creates an array of chars and initializes it with a
+ * specific char
  *
  * @size: size of array
  * @c: the specific char
  *
- * Return: pointer to array if success; NULL if fails
- * 
+ * Return: pointer to array if success; NULL if fails or size = 0
+ *
  */
+
+#include <stdio.h>
+#include <stdlib.h>
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *str;
 
+	/**
+	 * allocate size bytes for storing the char c size times in the
+	 * pointer str
+	 */
+	str = malloc(size * sizeof(char));
+
+	if (str == NULL || size == 0)
+		return (NULL);
+
+	i = 0;
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+
+	return (str);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,6 @@
+/**
+ * this is the header file main.h containing prototypes of all functions
+ * defined as part of the 0x0B project in the ALX SE Curriculum
+ */
+
+char *create_array(unsigned int size, char c);
